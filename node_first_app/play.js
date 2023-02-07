e8549671db304bdc65bf26ac2893bb7edbdc460f
syeef-test <syeefislam@yahoo.com>
2023-02-07 19:36:55 +0530

    product of two variable in arrow function done

--- node_first_app/play.js
+++ node_first_app/play.js
@@ -1,13 +1,24 @@
-const name = "Max";
-let age = 29;
-const hasHobbies = true;
+// const name = "Max";
+// let age = 29;
+// const hasHobbies = true;
 
-age = 30;
+// age = 30;
 
 
+// const summarizeUser = (userName,userAge,userHasHobby)=>{
+//     return ("Name is" + userName + ", age is " + userAge + " and the user has hobbies:" +userHasHobby);
+// }
 
-function summarizeUser(userName,userAge,userHasHobby){
-    return ("Name is" + userName + ", age is " + userAge + " and the user has hobbies:" +userHasHobby);
-}
+//const add = (a,b) => a+b;
+//const addOne = a => a + 1;
+//const addRandom = () => 1 + 2;
 
-console.log(summarizeUser(name,age,hasHobbies));
+//console.log(add(1,2));
+//console.log(addOne(1));
+//console.log(addRandom());
+
+const product = (a,b) => a * b;
+console.log(product(4,7));
+
+
+// console.log(summarizeUser(name,age,hasHobbies));
