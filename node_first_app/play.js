a68261e2f28df5a04a25eb3cc24ee0efaa538b00
syeef-test <syeefislam@yahoo.com>
2023-02-07 19:59:26 +0530

    object created

--- node_first_app/play.js
+++ node_first_app/play.js
@@ -1,24 +1,19 @@
-// const name = "Max";
-// let age = 29;
-// const hasHobbies = true;
-
-// age = 30;
-
-
-// const summarizeUser = (userName,userAge,userHasHobby)=>{
-//     return ("Name is" + userName + ", age is " + userAge + " and the user has hobbies:" +userHasHobby);
-// }
-
-//const add = (a,b) => a+b;
-//const addOne = a => a + 1;
-//const addRandom = () => 1 + 2;
-
-//console.log(add(1,2));
-//console.log(addOne(1));
-//console.log(addRandom());
-
-const product = (a,b) => a * b;
-console.log(product(4,7));
+// const person = {
+//     name:'kazi',
+//     age:29,
+//     greet(){
+//         console.log('Hi,I am ' + this.name);
+//     }
+// };
+// person.greet();
 
 
-// console.log(summarizeUser(name,age,hasHobbies));
+const student = {
+    name:'kazi',
+    age:29,
+    rollno:56,
+    studentDetails:function(){
+        console.log('Student name is:' + this.name + 'and roll no is:'+this.rollno);
+    }
+}
+student.studentDetails();
