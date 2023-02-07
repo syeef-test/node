42413dc5cc43b66f26e5b6bc0b432f3bdf242b44
syeef-test <syeefislam@yahoo.com>
2023-02-07 19:17:51 +0530

    let and const intro

--- node_first_app/play.js
+++ node_first_app/play.js
@@ -1,6 +1,9 @@
-var name = "Max";
-var age = 29;
-var hasHobbies = true;
+const name = "Max";
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
+
 
 
 function summarizeUser(userName,userAge,userHasHobby){
