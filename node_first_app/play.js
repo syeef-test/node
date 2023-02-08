556fccf2f90f092967771f2234896d3d6014e127
syeef-test <syeefislam@yahoo.com>
2023-02-08 08:28:42 +0530

    spread oper,rest oper,desut etc

--- node_first_app/play.js
+++ node_first_app/play.js
@@ -1,19 +1,65 @@
+//const hobbies = ['sports','coocking'];
+// for(let hobby of hobbies){
+//     console.log(hobby);
+// }
+
+// console.log(hobbies.map(hobby=>'Hobby:'+hobby));
+// console.log(hobbies);
+
+
+// array = ['apple', 'oranges' , '', 'mango', '' , 'lemon'];
+
+//  const newArray = array.map(fruit=>{
+//     if (fruit == ''){
+//         return 'empty string';
+//     }else{
+//         return fruit;
+//     }
+//  });
+
+//  console.log(newArray);
+
+
+//const newArray = hobbies.slice();//copy hobbies full portion into newArray if no argument passed
+// const newArray = [...hobbies];//spread operater copy hobbies to newArray,same for object
+// console.log(newArray);
+
+
+// const toArray = (...args)=>{        //rest use in function as argument
+//     return args;
+// };
+
+// console.log(toArray(1,2,3,4));
+
 // const person = {
 //     name:'kazi',
 //     age:29,
 //     greet(){
-//         console.log('Hi,I am ' + this.name);
+//         console.log('Hi,I am'+this.name);
 //     }
 // };
-// person.greet();
+
+// const printName = ({name})=>{
+//     console.log(name);
+// };
+// printName(person);
+
+// const {name,age} = person;
+// console.log(name,age);
 
 
-const student = {
-    name:'kazi',
-    age:29,
-    rollno:56,
-    studentDetails:function(){
-        console.log('Student name is:' + this.name + 'and roll no is:'+this.rollno);
-    }
-}
-student.studentDetails();
+
+// const obj1 = {'key1': 1};
+// const obj2 = { ...obj1};
+// if(obj2 === obj1){
+// console.log('same objects');
+// }
+// else{
+// console.log('different objects');//as referance is stored in obj
+// }
+
+
+// const obj1 = {'key1': 1 , 'key2' : 2};
+// const obj2 = { ...obj1, key1: 1000};
+// console.log(obj1);//{ key1: 1, key2: 2 }
+// console.log(obj2);//{ key1: 1000, key2: 2 }
