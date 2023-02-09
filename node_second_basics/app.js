71f60a7d1d6a0471c7413cf328a181e69412643a
syeef-test <syeefislam@yahoo.com>
2023-02-09 10:43:47 +0530

    reading and writing to file

--- node_second_basics/app.js
+++ node_second_basics/app.js
@@ -5,11 +5,16 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === "/") {
+
+    var fileData = fs.readFileSync("./message.txt").toString();
+    //console.log(fileData.toString());
+
     res.write("<html>");
     res.write("<head>");
     res.write("<title>Enter Message</title>");
     res.write("</head>");
     res.write("<body>");
+    res.write(`<h1>${fileData}</h1>`);
     res.write(
       '<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>'
     );
@@ -19,32 +24,32 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === "/message" && method === "POST") {
-    const body=[];
-    req.on('data',(chunk)=>{
-        console.log(chunk);
-        body.push(chunk);
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
     });
-    req.on('end',()=>{
-        const parsedBody = Buffer.concat(body).toString();
-        //console.log(parsedBody);
-        const message = parsedBody.split('=')[1];
-        fs.writeFileSync("message.txt", message);
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      //console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, () => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
     });
-    
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
 
-  res.setHeader('Content-Type','text/html');
-  res.write('<html>');
-  res.write('<head>');
-  res.write('<title>Home</title>');
-  res.write('</head>');
-  res.write('<body>');
-  res.write('<h1>Hello from node js server</h1>');
-  res.write('</body>');
-  res.write('</html>');
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head>");
+  res.write("<title>Home</title>");
+  res.write("</head>");
+  res.write("<body>");
+  res.write("<h1>Hello from node js server</h1>");
+  res.write("</body>");
+  res.write("</html>");
   res.end();
 });
 
