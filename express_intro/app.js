63383d1aacb3fc9e5267a5bea5523e6b1d97a6b5
syeef-test <syeefislam@yahoo.com>
2023-02-12 08:02:00 +0530

    bodyParser added

--- express_intro/app.js
+++ express_intro/app.js
@@ -1,17 +1,31 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware!');
-    next();//allow the request to continue to the next middleware in the line
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',(req,res,next)=>{
+    //console.log('This allways run');
+    next();
 });
 
-app.use((req,res,next)=>{
-    console.log('In another middleware!');
-    res.send({ key1: "value" });
+app.use('/add-product',(req,res,next)=>{
+    //console.log('In another middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
+});
+
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    //console.log('In another middleware!');
+    res.send('<h1>Hello Node Js</h1>');
 });
 
 app.listen(3000);
