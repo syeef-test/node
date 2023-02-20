3a58fd7433a98956735e64158dc8c0fb756dd2be
syeef-test <syeefislam@yahoo.com>
2023-02-20 20:39:26 +0530

    added sequelize and performed insert,fetch all and fetch singale product

--- express_dynamic_routing/controllers/admin.js
+++ express_dynamic_routing/controllers/admin.js
@@ -14,12 +14,17 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch(err => console.log(err));
+  Product.create({
+    title:title,
+    imageUrl:imageUrl,
+    price:price,
+    description:description
+  }).then(result=>{
+    //console.log(result);
+    console.log("created product");
+  }).catch(err=>{
+    console.log(err);
+  });
 };
 
 exports.getEditProduct = (req, res, next) => {
--- express_dynamic_routing/util/database.js
+++ express_dynamic_routing/util/database.js
@@ -1,10 +1,15 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    database: 'node_complete',
-    password: 'root'
-});
+const sequelize = new Sequelize('node_complete','root','root',{dialect:'mysql',host:'127.0.0.1'});
 
-module.exports = pool.promise();
+
+// const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     database: 'node_complete',
+//     password: 'root'
+// });
+// module.exports = pool.promise();
+
+module.exports = sequelize;
